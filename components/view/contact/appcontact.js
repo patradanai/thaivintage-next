836edf91d175d8351b989abe09e5a139ed985a57
Home <bloody_jojo@hotmail.com>
2021-07-05 20:03:10 +0700

    adjust date

--- components/view/contact/appcontact.js
+++ components/view/contact/appcontact.js
@@ -1,44 +1,44 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
-import Typography from "../../tyrography";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import Typography from '../../tyrography'
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    display: "flex",
-    overflow: "hidden"
+    display: 'flex',
+    overflow: 'hidden'
   },
   container: {
     marginTop: theme.spacing(10),
     marginBottom: theme.spacing(15),
-    position: "relative",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
   item: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    textAlign: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
     padding: theme.spacing(0, 5),
     marginLeft: theme.spacing(2)
   },
   images: {
     marginTop: theme.spacing(2),
-    display: "flex",
-    flexWrap: "wrap"
+    display: 'flex',
+    flexWrap: 'wrap'
   },
   imageSrc: {
     width: 48,
     height: 48,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginRight: theme.spacing(2),
-    "&:hover": {
+    '&:hover': {
       opacity: 0.5
     }
   },
@@ -47,36 +47,35 @@ const styles = theme => ({
     marginBottom: theme.spacing(2),
     lineHeight: 3
   }
-});
+})
 
-const Contact = props => {
-  const { classes } = props;
+const Contact = (props) => {
+  const { classes } = props
 
   const images = [
     {
-      url: "/images/facebook.png",
-      title: "facebook",
-      link:
-        "https://www.facebook.com/Thai-Vintage-Whitchurch-Restaurant-100411281542289"
+      url: '/images/facebook.png',
+      title: 'facebook',
+      link: 'https://www.facebook.com/Thai-Vintage-Whitchurch-Restaurant-100411281542289'
     },
-    { url: "/images/instagram.png", title: "instagram", link: "" },
-    { url: "/images/twitter.png", title: "twitter", link: "" }
-  ];
+    { url: '/images/instagram.png', title: 'instagram', link: '' },
+    { url: '/images/twitter.png', title: 'twitter', link: '' }
+  ]
   return (
     <section className={classes.root}>
       <Container className={classes.container}>
         <Grid container spacing={5}>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
-              <Typography variant="h4" align="center" component="h2">
+              <Typography variant='h4' align='center' component='h2'>
                 OPENING HOURS
               </Typography>
-              <Typography variant="h5" align="center" className={classes.Text}>
+              <Typography variant='h5' align='center' className={classes.Text}>
                 <pre>
-                  Open 6 days <br />
-                  Lunch: Tuesday- Sunday <br />
+                  {/* Open 6 days <br /> */}
+                  Lunch: Wed - Sunday <br />
                   12.00 pm - 3.00 pm <br />
-                  Dinner: Tuesday- Sunday <br />
+                  Dinner: Wed - Sunday <br />
                   5.30 pm - 10.00 pm
                 </pre>
               </Typography>
@@ -84,10 +83,10 @@ const Contact = props => {
           </Grid>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
-              <Typography variant="h4" align="center" component="h2">
+              <Typography variant='h4' align='center' component='h2'>
                 THAI VINTAGE WHITCHURCH
               </Typography>
-              <Typography variant="h5" align="center" className={classes.Text}>
+              <Typography variant='h5' align='center' className={classes.Text}>
                 <pre>
                   Old Mill, <br />
                   Park Avenue, <br />
@@ -113,10 +112,10 @@ const Contact = props => {
           </Grid>
           <Grid item xs={12} md={4}>
             <div className={classes.item}>
-              <Typography variant="h4" align="center">
+              <Typography variant='h4' align='center'>
                 CONTACT US
               </Typography>
-              <Typography variant="h5" className={classes.Text}>
+              <Typography variant='h5' className={classes.Text}>
                 <pre>
                   +44 1948 661800
                   <br />
@@ -124,7 +123,7 @@ const Contact = props => {
                   <br />
                   Email :
                   <br />
-                  <a href="mailto:info@thaivintagewhitchurch.co.uk">
+                  <a href='mailto:info@thaivintagewhitchurch.co.uk'>
                     info
                     <br />
                     @thaivintagewhitchurch.co.uk
@@ -136,11 +135,11 @@ const Contact = props => {
         </Grid>
       </Container>
     </section>
-  );
-};
+  )
+}
 
 Contact.prototype = {
   classes: PropTypes.object.isRequired
-};
+}
 
-export default withStyles(styles)(Contact);
+export default withStyles(styles)(Contact)
